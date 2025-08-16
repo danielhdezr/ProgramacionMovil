d64d925badafb63fb821440f46b91f3eb03b2f9f
danielhdezr <hernandezriosjuandaniel@gmail.com>
2025-08-15 22:08:32 -0700

    PrimerCommit

---
+++ index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>¡Mi primera app con React Native!</Text>
+      <Button
+        title="Presióname"
+        onPress={() => Alert.alert('¡Hola! Este es tu primer botón')}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+});
